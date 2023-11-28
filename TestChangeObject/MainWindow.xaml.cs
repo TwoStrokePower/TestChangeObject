df5b65bd19fdc69b6165284df6f45bf6ca9492e5
Dblr4ik 123 <egrik2807@mail.ru>
2023-11-28 20:22:30 +0300

    Добавьте файлы проекта.

---
+++ TestChangeObject/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestChangeObject
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string temp1 = "123213";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string Temp1 {
+            get => temp1;
+            set {  
+                temp1 = value;
+                OnPropertyChanged(nameof(Temp1));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
